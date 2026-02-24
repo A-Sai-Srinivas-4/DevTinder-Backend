09afbc3be9dd5083e9a67984b3070f08cf00c170
A-Sai-Srinivas-4 <asaisrinivas4@gmail.com>
2026-02-24 17:35:01 +0530

    added /auth path for authRouter

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ app.use(cookieParser()); // parse cookies
 
 
 // Use the routes
-app.use("/", authRouter);
+app.use("/auth", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
 app.use("/", userRouter);
